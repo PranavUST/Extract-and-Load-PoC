e7d410867d7a6cdad2c1c6a30926554806cca686
Akash-V-Nair <297026@ust.com>
2025-06-16 17:00:33 +0530

    frontend made consistent

--- extract-load-ui/src/app/app.ts
+++ extract-load-ui/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet, RouterModule } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { Footer } from './layout/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterModule, MatToolbarModule, MatButtonModule],
+  imports: [RouterOutlet, RouterModule, MatToolbarModule, MatButtonModule, Footer],
   templateUrl: './app.html',
   styleUrls: ['./app.scss']
 })
